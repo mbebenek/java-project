8ad17ac69bccc3bff7bb35494738e6e8049a7fc5
Brajan Nowak <brajan20cm@sexykamera.pl>
2017-05-30 19:16:29 +0200

    initial commint

---
+++ src/main/java/pl/edu/pwsz/student/demo/Demo.java
@@ -0,0 +1,14 @@
+package pl.edu.pwsz.student.demo;
+
+/**
+ * Created by Student on 2017-05-30.
+ */
+public class Demo {
+
+    public static void main(String[] args){
+        System.out.println("Michał");
+
+
+    }
+
+}
